0af7a98d804990cb29425e30c6538242d9a28985
glesiyi <71351240+glesiyi@users.noreply.github.com>
2020-09-30 13:28:02 +0300

    Add files via upload
    лаба2

---
+++ лаба 2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main()
+{
+    printf(" enter number\n a= ");
+    int a, b;
+    scanf_s("%i", &a);
+    for (int i = 31; i >= 0; i--)
+    {
+        b = ((a >> i) & 1);
+        printf("%i ", b);
+    }
+    return 0;
+
+}
