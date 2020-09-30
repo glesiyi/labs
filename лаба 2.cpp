0af6cbea21254320d83be446ff5f0a0b2e1076f9
glesiyi <71351240+glesiyi@users.noreply.github.com>
2020-09-30 13:35:20 +0300

    Update лаба 2.cpp

--- лаба 2.cpp
+++ лаба 2.cpp
@@ -4,7 +4,7 @@ int main()
     printf(" enter number\n a= ");
     int a, b;
     scanf_s("%i", &a);
-    for (int i = 31; i >= 0; i--)
+    for (int i = 31; i >= 0; i--) // цикл который переводит из 10 в 2
     {
         b = ((a >> i) & 1);
         printf("%i ", b);
