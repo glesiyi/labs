0c76107e9d0305bb6bed1006b967a37b69332bc3
glesiyi <71351240+glesiyi@users.noreply.github.com>
2020-10-14 09:44:36 +0300

    gleb lab 2,1

---
+++ лаба2,1.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    int a = 0, t = 0, d = 1;
+    setlocale(LC_ALL, "Russian");
+    printf(" Ввод числа\n ==> ");
+   
+    scanf_s ("%i", &a);
+   while (a)
+    {
+        t += (a % 2) * d;
+        a = a / 2;
+        d = d * 10;
+    }
+   printf ("%i ", t);
+   return 0;
+}
