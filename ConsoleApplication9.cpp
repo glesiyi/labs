5c42027d6fa280569122ac64fa654b764cf89a2f
glesiyi <71351240+glesiyi@users.noreply.github.com>
2020-10-27 21:31:09 +0300

    lab 3

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+int main()
+{
+    int n , k20 , k10, k5, k1, k;
+    printf(" enter number\n ==> ");
+    scanf_s("%d", &n);
+    k=0;
+    for (k20 = 0; k20 <= n / 20; k20++)
+    {
+        for (k10 = 0; k10 <= (n - 20 * k20) / 10; k10++)
+        {
+            for (k5 = 0; k5 <= (n - 20 * k20 - 10 * k10) / 5; k5++)
+            {
+
+                k1 = n - 20 * k20 - 10 * k10 - 5 * k5;
+                printf("20 - [%d] 10 - [%d] 5 - [%d] 1- [%d] \n", k20, k10, k5, k1);
+                k = k + 1;
+
+            }
+        }
+    }
+    printf("%i ", k);
+
+    return 0;
+}
+
+
+
+
+
+
+   
